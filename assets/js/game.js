9b851c6dcc08bcc9c62f0d8366b9a81d25220434
shaggy <simmons5family@gmail.com>
2020-10-05 01:11:25 -0500

    added var to store player name and print to console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! Javascript is running!");
+    // stores players name and prints it in console //
+var playerName =  window.prompt("What is your robot's name?");    
+console.log(playerName);
+
+    // creates a function named "fight" //
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
+
