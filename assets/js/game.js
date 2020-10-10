644aee9b929f10d52c01824c4785026d3b164021
shaggy <simmons5family@gmail.com>
2020-10-09 22:08:00 -0500

    DONE

--- assets/js/game.js
+++ assets/js/game.js
@@ -153,11 +153,19 @@ var startGame = function () {
 
 
 var endGame = function() {
-    //if player is still alive, player wins
-    if (playerInfo.health > 0) {
-        window.alert("Great Job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money);
     } else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score " + highScore + ". Maybe next time!");
     }
 
     //ask player if they'd like to play again
